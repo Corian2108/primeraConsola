d0c3a4c47fc7bf18e2f854d10747ca58647369a4
Jefferson Ruiz <jefferson.ruiz2108@gmail.com>
2020-06-24 21:29:26 -0500

    Creacion de primera consola

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace primeraConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hola" + args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Introduce tu nombre");
+                string nombre = Console.ReadLine();
+                if (nombre != "" && nombre !=null)
+                {
+                    Console.WriteLine("Bienvenido" + nombre);
+                    Console.ReadKey();
+                }
+                else
+                {
+                   Console.WriteLine("Para iniciar programa ingrese un nombre");
+                   Console.ReadKey();
+                }
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # primeraConsola
 Primer programa de consola desarrollado en lenguaje C#
+
+En el presente proyecto se demuestra un uso básico de asignación de variables y las funciones 
+ReadLine y Write line en una aplicación de consola desarrollada con lenguaje C#
+
+Para iniciar el programa ingrese un nombre
+Si ingresa una cadena null o vacía el programa dará un mensaje de error y se cerrará al presionar cualquier tecla
